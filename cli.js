90a1f3092750dc76174b9909ac16caa06861f860
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-01-20 12:11:32 +0200

    Teht 13.4

--- cli.js
+++ cli.js
@@ -1,5 +1,5 @@
 require('dotenv').config()
-const { Sequelize, Model, DataTypes } = require('sequelize')
+const { Sequelize, QueryTypes } = require('sequelize')
 
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialectOptions: {
@@ -10,39 +10,11 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
   }
 })
 
-class Blog extends Model {}
-Blog.init({
-  id: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true
-  },
-  author: {
-    type: DataTypes.TEXT
-  },
-  url: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  title: {
-    type: DataTypes.TEXT,
-    allowNull: false
-  },
-  likes: {
-    type: DataTypes.INTEGER,
-    defaultValue: 0
-  }
-}, {
-  sequelize,
-  underscored: true,
-  timestamps: false,
-  modelName: 'blog'
-})
-
 const printBlogs = async () => {
-  const blogs = await Blog.findAll()
+  const query = "SELECT * FROM blogs;"
+  const blogs = await sequelize.query(query, { type: QueryTypes.SELECT })
   blogs.forEach(blog => {
-    console.log(`${blog.dataValues.author ? blog.dataValues.author : 'unknown' }: '${blog.dataValues.title}', ${blog.dataValues.likes} likes`)
+    console.log(`${blog.author ? blog.author : 'unknown' }: '${blog.title}', ${blog.likes} likes`)
   })
 }
 
---
+++ index.js
@@ -0,0 +1,70 @@
+require('dotenv').config()
+const { Sequelize, Model, DataTypes } = require('sequelize')
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
+})
+
+class Blog extends Model {}
+Blog.init({
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  author: {
+    type: DataTypes.TEXT
+  },
+  url: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  title: {
+    type: DataTypes.TEXT,
+    allowNull: false
+  },
+  likes: {
+    type: DataTypes.INTEGER,
+    defaultValue: 0
+  }
+}, {
+  sequelize,
+  underscored: true,
+  timestamps: false,
+  modelName: 'blog'
+})
+
+Blog.sync()
+
+app.get('/api/health', (_req, res) => {
+  res.send('ok')
+})
+
+app.get('/api/blogs', async (_req, res) => {
+  const blogs = await Blog.findAll()
+  res.json(blogs)
+})
+
+app.post('/api/blogs', async (req, res) => {
+  console.log(req.body)
+  const blog = await Blog.create(req.body)
+  res.json(blog)
+})
+
+app.delete('/api/blogs/:id', async (req, res) => {
+  Blog.destroy({ where: { id: req.params.id } })
+  res.status(204).end()
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log('Server running on port', PORT)
+})
