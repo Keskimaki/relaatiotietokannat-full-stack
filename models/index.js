cd130c700a67530aefdb585d204ee9f2e1ebb184
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-01-21 13:35:00 +0200

    Teht 13.19

--- models/index.js
+++ models/index.js
@@ -1,7 +1,11 @@
 const Blog = require('./blog')
 const User = require('./user')
+const UserBlogs = require('./userBlogs')
 
 User.hasMany(Blog)
 Blog.belongsTo(User)
+
+User.belongsToMany(Blog, { through: UserBlogs, as: 'markedBlogs' })
+User.belongsToMany(User, { through: UserBlogs, as: 'usersMarked' })
 
 module.exports = { Blog, User }
