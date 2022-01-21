97c282a00787cbe8a001791ef97b38e2a3ad1db1
Miko Keskimäki <miko.keskimaki@helsinki.fi>
2022-01-21 14:37:37 +0200

    Teht 13.22

--- routes/readingList.js
+++ routes/readingList.js
@@ -1,11 +1,27 @@
 const express = require('express')
-const { ReadingList } = require('../models/index')
+const { ReadingList, User } = require('../models/index')
+const { tokenExtractor } = require('../utils/middlewares')
 
 const readingListRouter = express.Router()
 
 readingListRouter.post('/', async (req, res) => {
   const readingList = await ReadingList.create(req.body)
   res.json(readingList)
+})
+
+readingListRouter.put('/:id', tokenExtractor, async (req, res) => {
+  const readingList = await ReadingList.findByPk(req.params.id)
+  const user = await User.findByPk(req.decodedToken.id)
+
+  if (!readingList) {
+    res.send(404).end()
+  } else if (readingList.userId !== user.id) {
+    res.status(401).end()
+  } else {
+    readingList.read = req.body.read
+    await readingList.save()
+    res.json(readingList)
+  }
 })
 
 module.exports = readingListRouter
